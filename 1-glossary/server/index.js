1153d63ab089b739e1fc06aa8f03243001c399e6
gbmyt <gbmytabc1@gmail.com>
2022-12-23 11:14:01 -0500

    Server index.js setup

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,1 +1,12 @@
 require("dotenv").config();
+const path = require('path');
+
+const express = require('express');
+const app = express();
+var port = 3000;
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
