8998668ee583de86ab44a983fda23354764d2790
gbmyt <gbmytabc1@gmail.com>
2022-12-23 15:29:19 -0500

    Added clear form functionality after submitting

--- 1-glossary/client/src/components/SearchBar.jsx
+++ 1-glossary/client/src/components/SearchBar.jsx
@@ -10,15 +10,17 @@ const SearchBar = () => {
 	const search = (e) => {
 		e.preventDefault();
 		console.log('Searching:', searchTerm);
+		document.getElementById("searchFormInput").value = '';
 	};
 
 	return (
-		<div className="search">
-			<form id="search">
+		<div>
+			<form id="searchForm" >
 				<label htmlFor="search">Search:</label>
 				<input
 				type="text"
 				name="search"
+				id="searchFormInput"
 				placeholder="Type something..."
 				value={searchTerm}
 				onChange={setTerm}
