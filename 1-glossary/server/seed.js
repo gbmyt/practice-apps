5f88a5d3686397c399fbe31754bb43734a44dbf2
gbmyt <gbmytabc1@gmail.com>
2022-12-23 13:43:40 -0500

    Added database seed function

---
+++ 1-glossary/server/seed.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Word = require('./db');
+
+// mongoose.connect('mongodb://localhost/glossary', (err) => {
+// 	if (err) {
+// 		console.log("Connection Error: ", err);
+// 	} else {
+// 		console.log("Connection Open");
+// 	}
+// });
+
+const seedWords = [
+	{
+		name: 'antidisestablishmentarianism',
+		definition: 'opposition to the disestablishment of the Church of England.',
+		example: 'I never realized that you leaned toward antidisestablishmentarianism'
+	},
+	{
+		name: 'anachronistic',
+		definition: 'belonging or appropriate to an earlier period\, especially so as to seem conspicuously old-fashioned.',
+		example: 'she is rebelling against the anachronistic morality of her parents'
+	},
+	{
+		name: 'convivial',
+		definition: '(of an atmosphere or event) friendly, lively, and enjoyable.',
+		example: 'a convivial cocktail party'
+	},
+	{
+		name: 'pernicious',
+		definition: 'having a harmful effect, especially in a gradual or subtle way.',
+		example: 'the pernicious effects of air pollution'
+	},
+];
+
+// Seed Database Function
+const seedDB = async () => {
+	await Word.deleteMany({});
+	await Word.insertMany(seedWords);
+};
+
+seedDB().then(() => {
+	mongoose.connection.close();
+}).catch(err => {
+	console.log('Close Error: ', err);
+});
