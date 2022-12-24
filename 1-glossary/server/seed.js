5b67d870eac49477d117b0fe8a676d960f43c4f3
gbmyt <gbmytabc1@gmail.com>
2022-12-23 23:17:32 -0500

    Updated seed words

--- 1-glossary/server/seed.js
+++ 1-glossary/server/seed.js
@@ -4,25 +4,55 @@ const Word = require('./db').word;
 
 const seedWords = [
 	{
-		name: 'antidisestablishmentarianism',
-		definition: 'opposition to the disestablishment of the Church of England.',
+		name: 'Antidisestablishmentarianism',
+		definition: 'Opposition to the disestablishment of the Church of England.',
 		example: 'I never realized that you leaned toward antidisestablishmentarianism'
 	},
 	{
-		name: 'anachronistic',
-		definition: 'belonging or appropriate to an earlier period\, especially so as to seem conspicuously old-fashioned.',
-		example: 'she is rebelling against the anachronistic morality of her parents'
+		name: 'Nefarious',
+		definition: '(typically of an action or activity) wicked or criminal.',
+		example: 'The nefarious activities of the organized-crime syndicates'
 	},
 	{
-		name: 'convivial',
+		name: 'Anachronistic',
+		definition: 'Belonging or appropriate to an earlier period\, especially so as to seem conspicuously old-fashioned.',
+		example: 'She is rebelling against the anachronistic morality of her parents'
+	},
+	{
+		name: 'Pernicious',
+		definition: 'Having a harmful effect, especially in a gradual or subtle way.',
+		example: 'The pernicious effects of air pollution'
+	},
+	{
+		name: 'Convivial',
 		definition: '(of an atmosphere or event) friendly, lively, and enjoyable.',
-		example: 'a convivial cocktail party'
+		example: 'A convivial cocktail party'
 	},
 	{
-		name: 'pernicious',
-		definition: 'having a harmful effect, especially in a gradual or subtle way.',
-		example: 'the pernicious effects of air pollution'
+		name: 'Egregious',
+		definition: 'Outstandingly bad; shocking.',
+		example: 'Egregious abuses of copyright'
 	},
+	{
+		name: 'Draconian',
+		definition: 'Excessively harsh and severe.',
+		example: 'The new parking fines are positively draconian.'
+	},
+	{
+		name: 'Serendipity',
+		definition: 'The act of finding something valuable or interesting when you are not looking for it.',
+		example: 'It had to be serendipity that I found my dream house during a random drive in the country.'
+	},
+	{
+		name: 'Verisimilitude',
+		definition: 'The appearance of being true or real.',
+		example: 'The detail gives the novel some verisimilitude.'
+	},
+	{
+		name: 'Mellifluous',
+		definition: '(of a voice or words) sweet or musical; pleasant to hear.',
+		example: 'The voice was mellifluous and smooth.'
+	}
 ];
 
 // Seed Database Function
