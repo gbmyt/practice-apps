8392af91c46bc10846a4772e155a3f88c7086338
gbmyt <gbmytabc1@gmail.com>
2023-01-05 11:03:23 -0500

    Added MySQL schema file

---
+++ 2-checkout/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS checkout;
+
+CREATE DATABASE checkout;
+
+use checkout;
+
+CREATE TABLE IF NOT EXISTS responses (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(100) NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	addrOne INT NOT NULL,
+	addrTwo INT,
+	city VARCHAR(255) NOT NULL,
+	state VARCHAR(2) NOT NULL,
+	zip INT NOT NULL,
+	phone VARCHAR(10),
+	cc INT NOT NULL,
+	expiry VARCHAR(255) NOT NULL,
+	cvv INT NOT NULL,
+	billingZip INT NOT NULL,
+	session VARCHAR(255) NOT NULL,
+	UNIQUE(id),
+	UNIQUE(session)
+);
+
+DESCRIBE responses;
